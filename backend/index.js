02c1f1c0fa366e70dfcd8515dc1a68add17175f0
patiphan2000 <patiphan.sri@ku.th>
2022-01-29 20:51:41 +0700

    restaurants query && reviews

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import RestaurantsDAO from "./dao/restaurantsDAO.js"
+import ReviewsDAO from "./dao/reviewsDAO.js"
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
@@ -10,12 +11,12 @@ const port = process.env.port || 8000
 
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI
-    // ,
-    // {
-    //     poolSize: 50,
-    //     wtimeout: 2500,
-    //     useNewUrlParse: true
-    // }
+    ,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
+    }
     )
     .catch(err => {
         console.log(err.stack)
@@ -23,6 +24,7 @@ MongoClient.connect(
     })
     .then(async client => {
         await RestaurantsDAO.injectDB(client)
+        await ReviewsDAO.injectDB(client)
         app.listen(port, () => {
             console.log(`listening on port ${port}`)
         })
